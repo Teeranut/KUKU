ba4a6c4a044388721966bdfd4c9e70ebc7ef2436
Fifa thanthong <MIRRO@Fifas-MacBook-Air.local>
2018-10-12 10:39:30 +0700

    Yo.py V1.1.0

--- Yo.py
+++ Yo.py
@@ -1,12 +1,1 @@
-print("Yo!!!")
-print("Let's try Git XD")
-
-print("What?")
-
-print("Nani!")
-print("dafuq")
-print("lololololol")
-print("LOL!!!!!!")
 print("No U!!!")
-print("KIKI")
-print("Lmao")
