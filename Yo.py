c50974c8a85aea85606bfe79fa36264b431e4288
Fifa thanthong <MIRRO@Fifas-MacBook-Air.local>
2018-10-12 10:50:26 +0700

    Yo.py V1.1.0

--- Yo.py
+++ Yo.py
@@ -1,1 +1,2 @@
 print("No U!!!")
+print("dagofhegsrhsrhoirsghgoirsjgrhgoirejgrgrjgioerhgrjgowjgefrjgioeshgouwrgirssrgorwgsrehgiosehgiosehgiousrhguosrhgjksgjksbgjksbgjksebkjsbvjksbvkjsdbvjksdbvsdjkb")
