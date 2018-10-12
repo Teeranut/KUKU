292f5aad5c4b803afc181effbd918f909e7cb33b
Fifa thanthong <MIRRO@Fifas-MacBook-Air.local>
2018-10-12 10:54:09 +0700

    Yo.py V1.1.3

--- Yo.py
+++ Yo.py
@@ -1,6 +1,3 @@
 print("No U!!!")
-<<<<<<< HEAD
-print("dagofhegsrhsrhoirsghgoirsjgrhgoirejgrgrjgioerhgrjgowjgefrjgioeshgouwrgirssrgorwgsrehgiosehgiosehgiousrhguosrhgjksgjksbgjksbgjksebkjsbvjksbvkjsdbvjksdbvsdjkb")
-=======
 print("LOL is da best game ever!!!!!")
 >>>>>>> d879ecb47697483b6988217344e2283da74ed2d2
